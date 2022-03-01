738011d9443f4d9ee6f96cfe5ecef7eb27a7d98c
Javid Shiriyev <jshiriyev@utexas.edu>
2022-03-01 21:01:05 +0400

    created coordinates for transformation between coordinates

---
+++ mathbox/coordinates.py
@@ -0,0 +1,26 @@
+import numpy as np 
+
+if __name__ == "__main__":
+    import setup
+
+class coordinates():
+
+    def __init__(self):
+
+        pass
+
+    def cartesiantoradial(self):
+
+        pass
+
+    def radialtocartesian(self):
+
+        pass
+
+    def cartesiantospherical(self):
+
+        pass
+
+    def sphericaltocartesian(self):
+
+        pass
