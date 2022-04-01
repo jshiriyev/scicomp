dc680c0a4773b386f49a54ea3eecdd12de0ad36a
Javid Shiriyev <jshiriyev@utexas.edu>
2022-04-01 11:00:04 +0400

    ready

--- optimize/tutorials/multivariate_production.py
+++ optimize/tutorials/multivariate_production.py
@@ -6,12 +6,7 @@ from scipy.optimize import minimize
 from scipy.optimize import Bounds
 from scipy.optimize import LinearConstraint
 
-##def objective(xx):
-##    x = xx[0]
-##    y = xx[1]
-##    return (x-y)**2+1/3*(x+y-10)**2
-
-def objective(production_rate,profit_per_product):
+def profit(production_rate,profit_per_product):
 
     return -np.sum(production_rate*profit_per_product)
 
@@ -35,33 +30,9 @@ ub = [450, 250, 800, 450, 600]
 
 C = LinearConstraint(A,lb,ub)
 
-H = lambda x, v: np.zeros((3,3))
+H = lambda x,v: np.zeros((3,3))
 
-T = minimize(objective,np.zeros(3),args=(profit_per_product,),
+T = minimize(profit,np.zeros(3),args=(profit_per_product,),
              method='trust-constr',constraints=C,bounds=B,hess=H)
-    
-##xL = 0
-##xU = 8
-##
-##yL = 0
-##yU = 8
-##
-##x = np.linspace(xL,xU)
-##y = np.linspace(yL,yU)
-##
-##S = minimize(objective,(0,0),method="Powell")
-##
-##X,Y = np.meshgrid(x,y)
-##
-##Z = objective((X,Y))
-##
-##fig = plt.figure()
-##
-##ax = fig.add_subplot(111, projection='3d')
-##
-##ax.plot_surface(X,Y,Z)
-##
-##ax.set_xlabel('x-axis')
-##ax.set_ylabel('y-axis')
-##
-##plt.show()
+
+print(T)
